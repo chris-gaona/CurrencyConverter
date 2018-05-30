c0c4441de741bed810c92432308b99ed3ec45d69
Chris Gaona <thebeloved88@sbcglobal.net>
2018-05-30 08:55:18 -0700

    Creates initial Themes screen

--- App/index.js
+++ App/index.js
@@ -2,13 +2,17 @@ import React from 'react'
 import EStyleSheet from 'react-native-extended-stylesheet'
 // import Home from './Screens/Home'
 // import CurrencyList from './Screens/CurrencyList'
-import Options from './Screens/Options'
+// import Options from './Screens/Options'
+import Themes from './Screens/Themes'
 
 // disable yellow warnings
 console.disableYellowBox = true
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
+  $primaryOrange: '#D57866',
+  $primaryGreen: '#00BD9D',
+  $primaryPurple: '#9E768F',
   $white: '#fff',
   $border: '#e2e2e2',
   $inputText: '#797979',
@@ -16,4 +20,4 @@ EStyleSheet.build({
   $darkText: '#343434',
 })
 
-export default () => <Options />
+export default () => <Themes />
