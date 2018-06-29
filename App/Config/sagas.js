29f73b142c86e060f036348f02fdde3ef731e880
Chris Gaona <thebeloved88@sbcglobal.net>
2018-06-29 09:12:35 -0700

    Updates store from api response

--- App/Config/sagas.js
+++ App/Config/sagas.js
@@ -1,14 +1,43 @@
-import { takeEvery } from 'redux-saga/effects'
+import { takeEvery, select, call, put } from 'redux-saga/effects'
 
-// 1. swap currency
-// 2. change base currency
-// 3. initial app load
+import {
+  SWAP_CURRENCY,
+  CHANGE_BASE_CURRENCY,
+  GET_INITIAL_CONVERSION,
+  CONVERSION_RESULT,
+  CONVERSION_ERROR,
+} from '../actions/currencies'
 
-import { SWAP_CURRENCY, CHANGE_BASE_CURRENCY, GET_INITIAL_CONVERSION } from '../actions/currencies'
+const getLatestRate = currency => fetch(`https://fixer.handlebarlabs.com/latest?base=${currency}`)
 
 function* fetchLatestConversionRates(action) {
-  console.log('todo', action)
-  yield
+  let { currency } = action
+  if (currency === undefined) {
+    // yield says to wait for response before moving on
+    // select gives us access to current state
+    currency = yield select(state => state.currencies.baseCurrency)
+  }
+
+  try {
+    // call() allows our app to be more easily tested...it's not needed
+    const response = yield call(getLatestRate, currency)
+    const result = yield response.json()
+
+    if (result.error) {
+      // put() allows our app to be more easily tested...it's not needed
+      // emit an action
+      yield put({ type: CONVERSION_ERROR, error: result.error })
+    } else {
+      yield put({ type: CONVERSION_RESULT, result })
+    }
+  } catch (e) {
+    yield put({ type: CONVERSION_ERROR, error: e.message })
+  }
+
+  // getLatestRate(currency)
+  //   .then(res => res.json())
+  //   .then(res => console.log('res', res))
+  //   .catch(err => console.error(err))
 }
 
 export default function* rootSaga() {
